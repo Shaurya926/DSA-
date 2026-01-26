8def32c34063fbe124e44a6856c6e5397019cd14
Shaurya926 <santoshdubey7089@gmail.com>
2026-01-27 05:08:00 +0530

    DSA-LinkedList

---
+++ Linked_List/implementation.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Node structure
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to create a new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Insert at the end of circular linked list
+void insertEnd(struct Node** head, int data) {
+    struct Node* newNode = createNode(data);
+    if (*head == NULL) {
+        *head = newNode;
+        newNode->next = *head;  // Point to itself
+        return;
+    }
+    struct Node* temp = *head;
+    while (temp->next != *head) {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+    newNode->next = *head;
+}
+
+// Traverse and print circular linked list
+void traverse(struct Node* head) {
+    if (head == NULL) return;
+    struct Node* temp = head;
+    do {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    } while (temp != head);
+    printf("(back to head)\n");
+}
+
+int main() {
+    struct Node* head = NULL;
+    insertEnd(&head, 10);
+    insertEnd(&head, 20);
+    insertEnd(&head, 30);
+    insertEnd(&head, 40);
+
+    traverse(head);
+    return 0;
+}
